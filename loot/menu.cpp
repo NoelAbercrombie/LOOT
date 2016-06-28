306dcf96b536b80c3bdf3b8cc3b34bf94987e919
Pharap <pharap@hotmail.com>
2016-06-28 06:42:37 +0100

    Manually adjusted spacing for commas and operators

--- loot/menu.cpp
+++ loot/menu.cpp
@@ -17,9 +17,9 @@ void Menu::init()
 
 void Menu::step()
 {
-  logoAnim = max(logoAnim--,0);
+  logoAnim = max(logoAnim--, 0);
 
-  if(logoAnim==0) //if menu is ready
+  if(logoAnim == 0) //if menu is ready
   {
     if(ab->pushed(BTN_A))
     {
@@ -27,18 +27,21 @@ void Menu::step()
       {
         case 0:
         {
-          if(select==0) { ab->setState(stateGame);  };  //todo: link to a new/load game screen
-          if(select==1) { page = 2;};
-          if(select==2) { page = 3; select = 2; };
-        }; break;
+          if(select == 0) { ab->setState(stateGame);  };  //todo: link to a new/load game screen
+          if(select == 1) { page = 2;};
+          if(select == 2) { page = 3; select = 2; };
+          break;
+        } 
         case 2:
         {
           page = 0; select = 1;
-        }; break;
+          break;
+        } 
         case 3:
         {
           page = 0;
-        }; break;
+          break;
+        }
       }
     }
     if(ab->pushed(BTN_U))
@@ -50,7 +53,7 @@ void Menu::step()
       ++select;
     }
 
-    select = min(max(select,0),2); //seriously, no clamp()?
+    select = min(max(select, 0), 2); //seriously, no clamp()?
   }
 }
 void Menu::draw()
@@ -60,17 +63,17 @@ void Menu::draw()
     case 0:
     {
       //logo
-      if (logoAnim>0)
-        ab->drawBitmap(8,5-(64-logoAnim), imgTitle, 112, 54, 1);
+      if (logoAnim > 0)
+        ab->drawBitmap(8, 5 - (64 - logoAnim), imgTitle, 112, 54, 1);
       //menu text
-      ab->setCursor(16,logoAnim+8);
+      ab->setCursor(16, logoAnim + 8);
       ab->print(F("Play"));
-      ab->setCursor(16,logoAnim+16);
+      ab->setCursor(16, logoAnim + 16);
       ab->print(F("Config"));
-      ab->setCursor(16,logoAnim+24);
+      ab->setCursor(16, logoAnim + 24);
       ab->print(F("About"));
       //select cursor
-      ab->setCursor(8,logoAnim+8+(8*select));
+      ab->setCursor(8, logoAnim + 8 + (8 * select));
       ab->print(F(">"));
     };break;
     case 1: //Slots
@@ -79,12 +82,12 @@ void Menu::draw()
     };break;
     case 2: //options
     {
-      ab->setCursor(0,0);
+      ab->setCursor(0, 0);
       ab->print(F("Nothing to see here!"));
     };
     case 3: //About
     {
-      ab->setCursor(0,0);
+      ab->setCursor(0, 0);
       ab->print(F("Test string!"));
     }; break;
   };
