118245195f9b5af54111b47edce186a00f975acf
Pharap <pharap@hotmail.com>
2016-06-28 06:43:35 +0100

    Manually adjusted spacing for commas and operators

--- loot/player.h
+++ loot/player.h
@@ -12,16 +12,16 @@ class Player
     World * world;
 
   public:
-    Player(System & ab,World & world);
+    Player(System & ab, World & world);
 
     bool moved;
-    int8_t x,y; //needs to be signed so checking negative positions doesn't bug out
+    int8_t x, y; //needs to be signed so checking negative positions doesn't bug out
     Direction dir;
-    uint8_t hp,sp;
+    uint8_t hp, sp;
     char name[8];
 
     void init();
     void step();
-    void move(Direction,int8_t);
-    void jump(uint8_t,uint8_t);
+    void move(Direction, int8_t);
+    void jump(uint8_t, uint8_t);
 };
